3dd31c2dff7690cf2d6c7ca483c0ddea662e0108
Nick Pezza <pezza@hey.com>
2025-04-19 19:20:17 -0400

    Update to use ttags

--- etc/rbenv.d/install/ttags.bash
+++ etc/rbenv.d/install/ttags.bash
@@ -1,9 +1,9 @@
 if declare -Ff after_install >/dev/null; then
-  after_install generate_ctags
+  after_install generate_ttags
 fi
 
-generate_ctags() {
+generate_ttags() {
   if [ "$STATUS" -eq 0 ]; then
-    rbenv-ctags "$VERSION_NAME"
+    rbenv-ttags "$VERSION_NAME"
   fi
 }
